cf88cbe9c17a5955312ed82f53269a7fdff95fae
Tarun Tailor <taruntailor7@gmail.com>
2024-06-08 23:55:30 +0530

    starting writing code for server creation

--- pytohn/src/auth/server.py
+++ pytohn/src/auth/server.py
@@ -1,4 +1,10 @@
 import jwt, datetime, os
-from flask import FLASK, request
+from flask import Flask, request
 from flask_mysqldb import MySQL
 
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+print(server.config["MYSQL_HOST"])
