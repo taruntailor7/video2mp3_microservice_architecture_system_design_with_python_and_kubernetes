6a0fa10f765ba7f6c21ecfffacdfc26ccda7a261
Tarun Tailor <taruntailor7@gmail.com>
2024-06-13 23:35:12 +0530

    login and validate route created and create JWT function also created and main function condition also added with host and port

--- pytohn/src/auth/server.py
+++ pytohn/src/auth/server.py
@@ -36,8 +36,40 @@ def login():
             return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
     else:
         return "invalid credentials", 401
-        
+    
+@server.route("/validate", methods=["POST"])
+def validate():
+    encode_jwt = request.headers["Authorization"]
 
+    if not encode_jwt:
+        return "missing credentials", 401
+    
+    encode_jwt = encode_jwt.split(" ")[1]
+
+    try:
+        decode = jwt.decode(
+            encode_jwt,
+            os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+    
+    return decode, 200
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.datetime.utc) + datetime.timedelta(days=1),
+            'iat': datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+if __name__ == "__main__":
+    server.run(host='0.0.0.0', port=5000)
 
 
     
